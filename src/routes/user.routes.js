39176cf33037be8011c2646e8f5c6426db6d58ae
Flávio <f.augustosdn.dev@gmail.com>
2023-11-22 22:11:14 -0300

    feat: implementado upload de imagens para o banco de dados

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,19 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const UserControllers = require("../controllers/UserControllers");
+const UserAvatarController = require("../controllers/UserAvatarController");
 const ensureAthenticated = require("../middlewares/ensureAthenticated");
 
 const usersRouter = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const userControllers = new UserControllers();
+const userAvatarController = new UserAvatarController();
 
 usersRouter.post("/", userControllers.create);
 usersRouter.put("/update", ensureAthenticated, userControllers.update);
+usersRouter.patch("/avatar", ensureAthenticated, upload.single("avatar"), userAvatarController.update);
 
 module.exports = usersRouter;
--- src/server.js
+++ src/server.js
@@ -1,35 +1,31 @@
-require("express-async-errors")
+require("express-async-errors");
+const migrationsRun = require("./database/sqlite/migrations");
+const express = require("express");
+const route = require("./routes/index");
+const AppError = require("./utils/AppError");
+const uploadConfig = require("./configs/upload");
 
-const migrationsRun = require("./database/sqlite/migrations")
+const app = express();
 
-const express = require("express")
+migrationsRun();
 
-const route = require("./routes/index")
-
-const AppError = require("./utils/AppError")
-
-const app = express()
-
-migrationsRun()
-
-app.use(express.json())
-
-app.use(route)
+app.use(express.json());
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
+app.use(route);
 
 app.use((error, request, response, next) => {
-  if(error instanceof AppError) {
+  if (error instanceof AppError) {
     return response.status(error.statusCode).json({
-      status:"error",
-      message: error.message
-    })
+      status: "error",
+      message: error.message,
+    });
   }
 
   return response.status(500).json({
     status: "error",
-    message: "Internal server error"
-  })
+    message: "Internal server error",
+  });
+});
 
-})
-
-const port = 3000
-app.listen(port, console.log("Server is running"))
+const port = 3000;
+app.listen(port, console.log("Server is running"));
